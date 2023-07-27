abe6c45792660c558bd0eeb9acc9804c543e6255
Bibek Manandhar <manandhar.bibek01@gmail.com>
2023-07-27 09:31:06 +0545

    simple todo_app using node.js and ejs

---
+++ controllers/todoController.js
@@ -0,0 +1,18 @@
+var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
+
+
+module.exports = function(app) {
+    app.get('/todo', function(req, res) {
+        res.render('todo', {todos: data});
+    });
+    app.post('/todo', function(req, res) {
+        data.push(req.body);
+        res.json(data);
+    });
+    app.delete('/todo/:item', function(req, res) {
+        data = data.filter(function(todo) {
+            return todo.item.replace(/ /g, '-') !== req.params.item;
+        });
+        res.json(data);
+    });
+};
